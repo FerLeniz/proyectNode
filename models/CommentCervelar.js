ba317a693ea3e86086d751af672c5a47f8711366
Fernando Leniz <ferleniz93@gmail.com>
2021-09-19 19:47:54 -0300

    correccion

--- models/CommentCervelar.js
+++
@@ -1,8 +0,0 @@
-const mongoose=require('mongoose')
-
-const commentCerveSchema= new mongoose.Schema({
-    content:{ type: "String", required: true },
-})
-
-const CommentCervelar=mongoose.model('commentCerve',commentCerveSchema)
-module.exports=CommentCervelar
